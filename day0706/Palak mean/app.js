20932dd06675a0be432ae6112ac8f2dd666d40a8
Palaktejwani11 <135195533+Palaktejwani11@users.noreply.github.com>
2023-06-07 10:21:32 +0530

    Update app.js

--- day0706/Palak mean/app.js
+++ day0706/Palak mean/app.js
@@ -1,1 +1,15 @@
 
+const express=require("express")
+var app=express();
+app.use(express.static('public'));
+app.engine("html",require("ejs").renderFile);
+app.set("view engine","html")
+app.listen(1234);
+
+const homeRouter=require("./routes/home");
+const loginRouter=require("./routes/login");
+
+app.use("/",homeRouter);
+app.use("/login",loginRouter);
+//conditions in mongo
+//_id > 20          {id : {$gt;20}}
