7037759dc2771facce3e04f00ef1b0ebe2141ddf
Palaktejwani11 <135195533+Palaktejwani11@users.noreply.github.com>
2023-06-07 09:59:16 +0530

    Update app.js

--- day0706/app.js
+++ day0706/app.js
@@ -1,1 +1,11 @@
+const express=require("express");
+const app=express();
+app.listen(3000);
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+app.engine("html",require("ejs").renderFile);
+app.set("view engine","html");
+const homeRouter=require("./routes/home");
+app.use("/",homeRouter);
+
 
