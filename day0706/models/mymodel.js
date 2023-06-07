36ffca0356505fb2430e722b902a95e919e52ae1
Palaktejwani11 <135195533+Palaktejwani11@users.noreply.github.com>
2023-06-07 10:12:06 +0530

    Delete day0706/models directory

--- day0706/models/mymodel.js
+++
@@ -1,6 +0,0 @@
-const mongoose =require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/mydb");
-var con=mongoose.connection;
-var Schema=mongoose.Schema({enroll:String, name:String});
-var MyModel=mongoose.model("mycol",Schema);
-module.exports=MyModel;
