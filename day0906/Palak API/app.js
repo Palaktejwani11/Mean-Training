c3ecca710a1293b1726b7f9a5269631a1bf24f4f
Palaktejwani11 <135195533+Palaktejwani11@users.noreply.github.com>
2023-06-09 10:38:37 +0530

    Update app.js

--- day0906/Palak API/app.js
+++ day0906/Palak API/app.js
@@ -1,1 +1,23 @@
+const express=require("express");
+const app=express();
+app.listen(3000);
 
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded("extended:false"));
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine","html");
+
+const userRouter=require("./routes/users");
+app.use("/users",userRouter);const express=require("express");
+const app=express();
+app.listen(3000);
+
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded("extended:false"));
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine","html");
+
+const userRouter=require("./routes/users");
+app.use("/users",userRouter);
