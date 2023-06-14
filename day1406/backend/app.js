cc9d09f85e4b8eea0289e7a210d1f3c918fc9e3b
Palaktejwani11 <135195533+Palaktejwani11@users.noreply.github.com>
2023-06-14 10:46:18 +0530

    Update app.js

--- day1406/backend/app.js
+++ day1406/backend/app.js
@@ -1,1 +1,18 @@
+const express=require("express");
+const app=express();
+app.listen(3000);
 
+const bodyparser=require("body-parser")
+app.use(bodyparser.urlencoded({extended:false}));
+const cors=require("cors");
+app.use(cors());
+app.use(express.json());
+
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/mydb");
+var schema=mongoose.Schema({_id:String,password:String,name:String,role:String})   ;
+var User=mongoose.model("students",schema);
+app.get("/users",async (req,res)=>{
+    data= await User.find();
+    res.json(data);
+});               
