af9aeef8b31a074279feabf0f896860bebeed38a
Palaktejwani11 <135195533+Palaktejwani11@users.noreply.github.com>
2023-06-21 10:05:24 +0530

    Add files via upload

---
+++ 2106/myapp3/myapp/src/App.js
@@ -0,0 +1,31 @@
+import About from './About';
+import Login from './Login'; //not using {} cuz we used export default in login.js
+import Register from './Register';
+import Contact from './Contact';
+import { BrowserRouter, Route,Routes,Link } from 'react-router-dom';
+import MyClass from './MyClass';
+
+function App() {
+  return(
+  <>
+<Link to="/" >Home</Link> |
+<Link to="/reg" >Register</Link>|
+<Link to="login/" >Login</Link>|
+<Link to="/contact" >Contact</Link>|
+<Link to="/about" >About</Link>|
+<Link to="/my" >Class Component</Link>|
+<br/>
+  
+
+   <Routes>
+    <Route path="/login" element=<Login/> />
+    <Route path="/reg" element=<Register/> />
+    <Route path="/about" element=<About/> />
+    <Route path="/contact" element=<Contact/> />
+    <Route path="/my" element=<MyClass/> />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ 2106/myapp3/myapp/src/MyClass.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+ class MyClass extends Component
+{
+    render()
+    { return(<>Class Component</>)
+}
+}
+export default MyClass;
